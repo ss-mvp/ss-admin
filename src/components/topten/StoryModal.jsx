43faee0da954fe8b5174a6def81a496bb18e2d3d
Alexander Sierra <alexandercsierra@gmail.com>
2020-08-20 18:51:29 -0400

    gave modals unique ids

--- src/components/topten/StoryModal.jsx
+++ src/components/topten/StoryModal.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
+import React from 'react';
 
 export default function StoryModal(props) {
-    return (
-        <>
-        <div className="modal fade" id="storyModal" tabIndex="-1" role="dialog" aria-hidden="true">
-            <div className="modal-dialog modal-dialog-centered">
-                <div className="modal-content">
-                    <div className="title bg-primary text-white">
-                        <h4 className="p-3">Story</h4>
-                    </div>
-                    <div className="modal-body">
-                        <img src={props.submission.image} alt='submission' />
-                    </div>
-                </div>
+  return (
+    <>
+      <div
+        className="modal fade"
+        id={`storyModal ${props.submission.id}`}
+        tabIndex="-1"
+        role="dialog"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="title bg-primary text-white">
+              <h4 className="p-3">Story</h4>
             </div>
-        </div>    
-        </>
-    )
+            <div className="modal-body">
+              <img src={props.submission.image} alt="submission" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/components/topten/TopTenBar.jsx
+++ src/components/topten/TopTenBar.jsx
@@ -1,22 +1,33 @@
-import React, { useState, useEffect } from 'react'
-import StoryModal from './StoryModal'
-import VoteButton from './buttons/VoteButton'
-import FlagButton from './buttons/FlagButton'
+import React, { useState, useEffect } from 'react';
+import StoryModal from './StoryModal';
+import VoteButton from './buttons/VoteButton';
+import FlagButton from './buttons/FlagButton';
 
 export function TopTenBar({ submission, hasAdminVoted }) {
-
-    return (
-        <>
-            <tr>
-                <td>{ submission.username }</td>
-                <td>Status</td>
-                <td style={{ marginLeft: '30%', display: 'flex' }}>
-                    <button className="btn btn-outline-primary m-2 px-4" data-toggle="modal" data-target="#storyModal">View</button>
-                    <StoryModal submission={ submission } />
-                    <VoteButton story_id={ submission.id } user_id={ submission.userId } topThree={submission.topThree} vote={submission.vote} hasAdminVoted={hasAdminVoted}/>
-                    <FlagButton submission={ submission } />
-                </td>
-            </tr>
-        </>
-    )
+  return (
+    <>
+      <tr>
+        <td>{submission.username}</td>
+        <td>Status</td>
+        <td style={{ marginLeft: '30%', display: 'flex' }}>
+          <button
+            className="btn btn-outline-primary m-2 px-4"
+            data-toggle="modal"
+            data-target={`#storyModal ${submission.id}`}
+          >
+            View
+          </button>
+          <StoryModal key={submission.id} submission={submission} />
+          <VoteButton
+            story_id={submission.id}
+            user_id={submission.userId}
+            topThree={submission.topThree}
+            vote={submission.vote}
+            hasAdminVoted={hasAdminVoted}
+          />
+          <FlagButton submission={submission} />
+        </td>
+      </tr>
+    </>
+  );
 }
