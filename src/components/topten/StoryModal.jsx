3814f409b0e7dc466567e49b92e27d5a7e350c9b
lgv-0 <loganvan@att.net>
2020-10-09 14:20:01 -0400

    Setup new image grabbing URL for topten

--- src/components/topten/StoryModal.jsx
+++ src/components/topten/StoryModal.jsx
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import { AxiosWithAuth } from '../../utils';
 
 export default function StoryModal(props) {
+  let [SubData, sSubData] = useState();
+  useEffect(() => {
+    async function GetIt(id)
+    {
+      AxiosWithAuth().get(`admin/image/${id}`, { responseType: "arraybuffer" })
+        .then((response) =>
+        {
+          let image = btoa(
+            new Uint8Array(response.data)
+              .reduce((data, byte) => data + String.fromCharCode(byte), '')
+          );
+          sSubData(`data:${response.headers['content-type'].toLowerCase()};base64,${image}`);
+        });
+    }
+    GetIt(props.submission.id);
+  }, []);
+
+  if (!SubData)
+    return(<>Loading...</>)
+
   return (
     <>
       <div
@@ -16,7 +38,7 @@ export default function StoryModal(props) {
               <h4 className="p-3">Story</h4>
             </div>
             <div className="modal-body">
-              <img src={props.submission.image} alt="submission" />
+              <img src={SubData} alt="submission" />
             </div>
           </div>
         </div>
