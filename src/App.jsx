79bfb313f146d50fc57ef736a068975c72968e18
Benjamin Griffin <49883273+B-Griffinn@users.noreply.github.com>
2020-12-30 13:55:24 -0800

    Feature/rotate images (#16)
    * Added image rotation to the story modal
    
    * Ready to test on live site

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom'
-import { PromptPage, TopTenPage, Home, Video, WinnersPage} from './pages';
-import { PrivateRoute } from './utils'
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import { PromptPage, TopTenPage, Home, Video, WinnersPage } from "./pages";
+import { PrivateRoute } from "./utils";
 
 function App() {
   return (
     <div className="maindiv">
       <Switch>
-        <Route exact path="/" component={ Home } />
-        <PrivateRoute path="/topten" component={ TopTenPage } />
-        <PrivateRoute path="/prompts" component={ PromptPage } />
+        <Route exact path="/" component={Home} />
+
+        <PrivateRoute path="/topten" component={TopTenPage} />
+        <PrivateRoute path="/prompts" component={PromptPage} />
         <PrivateRoute path="/video" component={Video} />
         <PrivateRoute path="/winners" component={WinnersPage} />
       </Switch>
--- src/components/topten/StoryModal.jsx
+++ src/components/topten/StoryModal.jsx
@@ -1,27 +1,42 @@
-import React, { useEffect } from 'react';
-import { useState } from 'react';
-import { AxiosWithAuth } from '../../utils';
+import React, { useState, useEffect } from "react";
+import { AxiosWithAuth } from "../../utils";
 
 export default function StoryModal(props) {
-  let [SubData, sSubData] = useState();
+  // Rotation State
+  const [imageRotation, setImageRotation] = useState(0);
+  // subData State
+  let [SubData, setSubData] = useState();
+
+  // Rotate Functions
+  const rotateRight = () => setImageRotation((cur) => (cur >= 3 ? 0 : cur + 1));
+  const rotateLeft = () => setImageRotation((cur) => (cur <= 0 ? 3 : cur - 1));
+
   useEffect(() => {
-    async function GetIt(id)
-    {
-      AxiosWithAuth().get(`admin/image/${id}`, { responseType: "arraybuffer" })
-        .then((response) =>
-        {
+    async function GetIt(id) {
+      AxiosWithAuth()
+        .get(`admin/image/${id}`, { responseType: "arraybuffer" })
+        .then((response) => {
           let image = btoa(
-            new Uint8Array(response.data)
-              .reduce((data, byte) => data + String.fromCharCode(byte), '')
+            new Uint8Array(response.data).reduce(
+              (data, byte) => data + String.fromCharCode(byte),
+              ""
+            )
           );
-          sSubData(`data:${response.headers['content-type'].toLowerCase()};base64,${image}`);
+          setSubData(
+            `data:${response.headers[
+              "content-type"
+            ].toLowerCase()};base64,${image}`
+          );
+        })
+        .catch((err) => {
+          console.log(err);
+          return;
         });
     }
     GetIt(props.submission.id);
   }, []);
 
-  if (!SubData)
-    return(<>Loading...</>)
+  if (!SubData) return <>Loading...</>;
 
   return (
     <>
@@ -38,7 +53,42 @@ export default function StoryModal(props) {
               <h4 className="p-3">Story</h4>
             </div>
             <div className="modal-body">
-              <img src={SubData} alt="submission" />
+              <img
+                src={SubData}
+                alt="submission"
+                style={{ transform: `rotate(${imageRotation * 90}deg)` }}
+              />
+            </div>
+            <div
+              className="modal-controls"
+              style={{
+                width: "100%",
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              <img
+                src={require("../../assets/rotate-left.png")}
+                alt="rotate left"
+                style={{
+                  width: "50px",
+                  zIndex: "200",
+                  padding: "10px",
+                  cursor: "pointer",
+                }}
+                onClick={rotateLeft}
+              />
+              <img
+                src={require("../../assets/rotate-right.png")}
+                alt="rotate right"
+                style={{
+                  width: "50px",
+                  zIndex: "200",
+                  padding: "10px",
+                  cursor: "pointer",
+                }}
+                onClick={rotateRight}
+              />
             </div>
           </div>
         </div>
